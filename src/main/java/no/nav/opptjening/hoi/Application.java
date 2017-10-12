bbbabb0e08a82e9b7ae9b88924434e7c6774d08f
David Steinsland <github@davidsteinsland.net>
2017-10-12 15:42:07 +0200

    Adding prometheus and nais endpoints

--- src/main/java/no/nav/opptjening/hoi/Application.java
+++ src/main/java/no/nav/opptjening/hoi/Application.java
@@ -1,9 +1,13 @@
 package no.nav.opptjening.hoi;
 
+import io.prometheus.client.spring.boot.EnablePrometheusEndpoint;
+import io.prometheus.client.spring.boot.EnableSpringBootMetricsCollector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnablePrometheusEndpoint
+@EnableSpringBootMetricsCollector
 public class Application {
 
     public static void main(String[] args) {
