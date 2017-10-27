32eba7a8d997f6c045295b7467ef7bdb6c565262
David Steinsland <github@davidsteinsland.net>
2017-10-27 09:56:50 +0200

    Added Confluent and Avro integration

--- src/main/java/no/nav/opptjening/hoi/hendelser/InntektHendelseConsumer.java
+++ src/main/java/no/nav/opptjening/hoi/hendelser/InntektHendelseConsumer.java
@@ -1,7 +1,9 @@
 package no.nav.opptjening.hoi.hendelser;
 
+import no.nav.opptjening.schema.InntektHendelse;
 import no.nav.opptjening.skatt.api.pgi.Inntekter;
 import no.nav.opptjening.skatt.dto.InntektDto;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -19,11 +21,12 @@ public class InntektHendelseConsumer {
     }
 
     @KafkaListener(topics = "tortuga.inntektshendelser")
-    public void mottaHendelse(InntektKafkaHendelseDto hendelse) {
+    public void mottaHendelse(ConsumerRecord<String, InntektHendelse> record) {
+        InntektHendelse hendelse = record.value();
         LOG.info("HOI haandterer hendelse={}", hendelse);
 
         try {
-            InntektDto inntekt = inntekter.hentInntekt(hendelse.getGjelderPeriode(), hendelse.getIdentifikator());
+            InntektDto inntekt = inntekter.hentInntekt(hendelse.getGjelderPeriode().toString(), hendelse.getIdentifikator().toString());
             LOG.info("HOI sender inntekt='{}'", inntekt);
         } catch (Exception e) {
             LOG.error("Her skjedde det noe galt ja", e);
