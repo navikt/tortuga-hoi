1a8010cc12ddc5b20cccbf85139000db83b5dd7f
David Steinsland <github@davidsteinsland.net>
2017-11-30 15:25:35 +0100

    Producing hendelser with key and log which partitions is assigend to HOI

--- src/main/java/no/nav/opptjening/hoi/hendelser/InntektHendelseConsumer.java
+++ src/main/java/no/nav/opptjening/hoi/hendelser/InntektHendelseConsumer.java
@@ -5,16 +5,19 @@ import no.nav.opptjening.schema.Inntekt;
 import no.nav.opptjening.skatt.api.pgi.InntektDto;
 import no.nav.opptjening.skatt.api.pgi.Inntekter;
 import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRebalanceListener;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.TopicPartition;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.actuate.metrics.CounterService;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.util.Collection;
 import java.util.Collections;
 
 @Component
@@ -36,11 +39,22 @@ public class InntektHendelseConsumer {
 
         this.counterService.reset("inntektshendelser.received");
         this.counterService.reset("inntektshendelser.processed");
+
+        hendelseConsumer.subscribe(Collections.singletonList("tortuga.inntektshendelser"), new ConsumerRebalanceListener() {
+            @Override
+            public void onPartitionsRevoked(Collection<TopicPartition> partitions) {
+                LOG.info("Partition revoked: {}", partitions);
+            }
+
+            @Override
+            public void onPartitionsAssigned(Collection<TopicPartition> partitions) {
+                LOG.info("Partitions assigned: {}", partitions);
+            }
+        });
     }
 
     @Scheduled(fixedDelay = 5000, initialDelay = 5000)
     public void poll() {
-        hendelseConsumer.subscribe(Collections.singletonList("tortuga.inntektshendelser"));
         ConsumerRecords<String, Hendelse> hendelser = hendelseConsumer.poll(500);
 
         for (int i = 0; i < hendelser.count(); i++) {
