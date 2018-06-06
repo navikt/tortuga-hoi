c827b8ee8ea2d11fc154b1b37a1625241cb1f210
Marius Eriksen <Marius.Eriksen@nav.no>
2018-06-06 13:17:54 +0200

    Refactored test to use correct setters, and casts to long in the asserts

--- src/test/java/no/nav/opptjening/hoi/PensjonsgivendeInntektMapperTest.java
+++ src/test/java/no/nav/opptjening/hoi/PensjonsgivendeInntektMapperTest.java
@@ -2,7 +2,8 @@ package no.nav.opptjening.hoi;
 
 import no.nav.opptjening.hoi.PensjonsgivendeInntektMapper;
 import no.nav.opptjening.schema.PensjonsgivendeInntekt;
-import no.nav.opptjening.skatt.schema.BeregnetSkatt;
+
+import no.nav.opptjening.schema.skatt.BeregnetSkatt;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -10,15 +11,14 @@ public class PensjonsgivendeInntektMapperTest {
     @Test
     public void toPensjonsgivendeInntekt() throws Exception {
         BeregnetSkatt beregnetSkatt = BeregnetSkatt.newBuilder()
-                .withPersonidentifikator("12345678911")
-                .withInntektsaar("2018")
-                .withPersoninntektLoenn(5678)
-                .withPersoninntektFiskeFangstFamiliebarnehage(7890)
-                .withPersoninntektNaering(8901)
-                .withPersoninntektBarePensjonsdel(9012)
-                .withSvalbardLoennLoennstrekkordningen(89012)
-                .withSvalbardPersoninntektNaering(123456)
-                .withSkjermet(false)
+                .setPersonidentifikator("12345678911")
+                .setInntektsaar("2018")
+                .setPersoninntektLoenn(5678)
+                .setPersoninntektFiskeFangstFamiliebarnehage(7890)
+                .setPersoninntektNaering(8901)
+                .setPersoninntektBarePensjonsdel(9012)
+                .setSvalbardLoennLoennstrekkordningen(89012)
+                .setSvalbardPersoninntektNaering(123456)
                 .build();
 
         PensjonsgivendeInntekt pensjonsgivendeInntekt = new PensjonsgivendeInntektMapper().toPensjonsgivendeInntekt(beregnetSkatt);
@@ -26,13 +26,13 @@ public class PensjonsgivendeInntektMapperTest {
         Assert.assertEquals(beregnetSkatt.getPersonidentifikator(), pensjonsgivendeInntekt.getPersonidentifikator());
         Assert.assertEquals(beregnetSkatt.getInntektsaar(), pensjonsgivendeInntekt.getInntektsaar());
 
-        Assert.assertEquals(beregnetSkatt.getPersoninntektLoenn(), (long)pensjonsgivendeInntekt.getFastlandsinntekt().getPersoninntektLoenn());
-        Assert.assertEquals(beregnetSkatt.getPersoninntektFiskeFangstFamiliebarnehage(), (long)pensjonsgivendeInntekt.getFastlandsinntekt().getPersoninntektFiskeFangstFamiliebarnehage());
-        Assert.assertEquals(beregnetSkatt.getPersoninntektNaering(), (long)pensjonsgivendeInntekt.getFastlandsinntekt().getPersoninntektNaering());
-        Assert.assertEquals(beregnetSkatt.getPersoninntektBarePensjonsdel(), (long)pensjonsgivendeInntekt.getFastlandsinntekt().getPersoninntektBarePensjonsdel());
+        Assert.assertEquals((long)beregnetSkatt.getPersoninntektLoenn(), (long)pensjonsgivendeInntekt.getFastlandsinntekt().getPersoninntektLoenn());
+        Assert.assertEquals((long)beregnetSkatt.getPersoninntektFiskeFangstFamiliebarnehage(), (long)pensjonsgivendeInntekt.getFastlandsinntekt().getPersoninntektFiskeFangstFamiliebarnehage());
+        Assert.assertEquals((long)beregnetSkatt.getPersoninntektNaering(), (long)pensjonsgivendeInntekt.getFastlandsinntekt().getPersoninntektNaering());
+        Assert.assertEquals((long)beregnetSkatt.getPersoninntektBarePensjonsdel(), (long)pensjonsgivendeInntekt.getFastlandsinntekt().getPersoninntektBarePensjonsdel());
 
-        Assert.assertEquals(beregnetSkatt.getSvalbardLoennLoennstrekkordningen(), (long)pensjonsgivendeInntekt.getSvalbardinntekt().getSvalbardLoennLoennstrekkordningen());
-        Assert.assertEquals(beregnetSkatt.getSvalbardPersoninntektNaering(), (long)pensjonsgivendeInntekt.getSvalbardinntekt().getSvalbardPersoninntektNaering());
+        Assert.assertEquals((long)beregnetSkatt.getSvalbardLoennLoennstrekkordningen(), (long)pensjonsgivendeInntekt.getSvalbardinntekt().getSvalbardLoennLoennstrekkordningen());
+        Assert.assertEquals((long)beregnetSkatt.getSvalbardPersoninntektNaering(), (long)pensjonsgivendeInntekt.getSvalbardinntekt().getSvalbardPersoninntektNaering());
 
     }
 
