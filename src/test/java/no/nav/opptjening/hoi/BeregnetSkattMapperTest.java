5e1c2c07308fe053483fa5dae70df856e77ba712
Marius Eriksen <Marius.Eriksen@nav.no>
2018-06-11 14:03:06 +0200

    Added test to verify that we catch when a beregnet skatt for a given year was not found

--- src/test/java/no/nav/opptjening/hoi/BeregnetSkattMapperTest.java
+++ src/test/java/no/nav/opptjening/hoi/BeregnetSkattMapperTest.java
@@ -10,6 +10,7 @@ import org.junit.Rule;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 public class BeregnetSkattMapperTest {
 
@@ -40,6 +41,14 @@ public class BeregnetSkattMapperTest {
         assertEquals(expectedBeregnetSkatt, transformedHendelse);
     }
 
+    @Test
+    public void transformReturnsNullWhenFantIkkeBeregnetSkattExceptionIsThrown() {
+        createMockApi();
+        Hendelse hendelse = new Hendelse(0L, "11987654321", "2014");
+        BeregnetSkatt transformedHendelse = beregnetSkattMapper.transform(hendelse);
+        assertNull(transformedHendelse);
+    }
+
     private void createMockApi() {
         WireMock.stubFor(WireMock.get(WireMock.urlPathEqualTo("/nav/2018/12345678911"))
                 .withHeader("X-Nav-Apikey", WireMock.equalTo("foobar"))
@@ -50,5 +59,13 @@ public class BeregnetSkattMapperTest {
                         "  \"skjermet\": false,\n" +
                         "  \"skatteoppgjoersdato\": \"2018-06-06\"\n" +
                         "}")));
+
+        WireMock.stubFor(WireMock.get(WireMock.urlPathEqualTo("/nav/2014/11987654321"))
+                .withHeader("X-Nav-Apikey", WireMock.equalTo("foobar"))
+                .willReturn(WireMock.notFound().withBody("{\n" +
+                        "  \"kode\": \"BSA-006\",\n" +
+                        "  \"melding\": \"Fant ikke Beregnet Skatt for gitt inntektsår og identifikator\",\n" +
+                        "  \"korrelasjonsid\": \"13a865f5-28f9-47db-9abd-ab78977c79fe\"\n" +
+                "}")));
     }
 }
