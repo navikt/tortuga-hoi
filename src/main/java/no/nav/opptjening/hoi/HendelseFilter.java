7087208c9bf5aedc1afd66b80c41f364c78127cf
Marius Eriksen <Marius.Eriksen@nav.no>
2018-08-31 11:00:56 +0200

    Changed appconfig to use 3 nodes. Added metrics for summarizing the amount of hendelser recieved

--- src/main/java/no/nav/opptjening/hoi/HendelseFilter.java
+++ src/main/java/no/nav/opptjening/hoi/HendelseFilter.java
@@ -1,12 +1,18 @@
 package no.nav.opptjening.hoi;
 
+import io.prometheus.client.Counter;
 import no.nav.opptjening.schema.skatt.hendelsesliste.Hendelse;
 
 public class HendelseFilter  {
 
+    private static final Counter inntektsHendelserRecieved = Counter.build()
+            .name("beregnet_skatt_hendelser_mottatt")
+            .help("Antall hendelser mottatt").register();
+
     private static final int EARLIEST_VALID_HENDELSE_YEAR = 2017;
 
     public static boolean testThatHendelseIsFromValidYear(String key, Hendelse hendelse) {
+        inntektsHendelserRecieved.inc();
         return Integer.parseInt(hendelse.getGjelderPeriode()) >= EARLIEST_VALID_HENDELSE_YEAR;
     }
 }
